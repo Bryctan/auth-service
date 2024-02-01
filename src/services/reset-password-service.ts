3be92677d876cace3b76269acfad68f09fed79ca
Bryctan <juanchoquintero1004@gmail.com>
2024-02-01 00:54:48 -0500

    first commit

--- src/services/reset-password-service.ts
+++ src/services/reset-password-service.ts
@@ -1,8 +1,9 @@
-import connection from "../config/db-config";
+import { createConnection } from '../config/db-config';
 import bcrypt from "bcrypt";
 
 export const verifyGrocer = (emailGrocer: any, callback: any) => {
   const grocerExisting = "call getGrocerEmailExist(?,@message_text);";
+  const connection = createConnection();
   try {
     connection.query(
       grocerExisting,
@@ -21,6 +22,7 @@ export const verifyGrocer = (emailGrocer: any, callback: any) => {
 
 export const verifyCompany = (emailProvider: any, callback: any) => {
   const providerExisting = "call getCompany_EmailExist (?,@message_text);";
+  const connection = createConnection();
   try {
     connection.query(
       providerExisting,
@@ -43,6 +45,7 @@ export const updatePassword = async (
   password: any,
   callback: any
 ) => {
+  const connection = createConnection();
   const password_hash = await bcrypt.hash(password, 10);
 
   const data: { email: string; password: string }[] = [
